26c0e45d26d08ac8cf2f76f1ea5ccecc66e3fe0d
cuiyang36 <wangyang19901026@gmail.com>
2015-03-02 23:24:16 -0500

    add new problem

--- src/Exercise/JosephusCircle.java
+++ src/Exercise/JosephusCircle.java
@@ -11,7 +11,46 @@ package Exercise;
 public class JosephusCircle {
 	
 	public static ListNode createCycle(int length){
-		
+		if (length < 1){
+			return null;
+		}
+		ListNode head = new ListNode(0);
+		ListNode tail = head;
+		for (int i = 1; i < length; i++){
+			tail.next = new ListNode(i);
+			//System.out.println(tail.next.val + " created");
+			tail = tail.next;
+		}
+		tail.next = head;
+		//System.out.println(tail.val);
+		//System.out.println(tail.next.val);
+		return tail;
+	}
+	
+	public static ListNode killToOne(int length, int k){
+		ListNode tail;
+		tail = createCycle(length);
+		if (k <= 0 || tail == null){
+			return null;
+		}
+		k -= 1;
+		ListNode curr = tail;
+		while (length > 1){
+			int num = k % length;
+			while (num > 0){
+				curr = curr.next;
+				num -= 1;
+			}
+			System.out.println("kill: " + curr.next.val);
+			curr.next = curr.next.next;
+			length -= 1;
+		}
+		return curr;
+	}
+	
+	public static void main(String[] args){
+		ListNode rst = killToOne(1000, 10);
+		System.out.println(rst.val + " left!");
 	}
 
 }
